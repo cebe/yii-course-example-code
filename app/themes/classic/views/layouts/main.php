f6134e1b623e25bd59bd8891ca1dbbd9528d9ad9
Mila Brandt <mail@mila-brandt.de>
2013-07-20 13:08:26 +0200

    adding themes

---
+++ app/themes/classic/views/layouts/main.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+
+		<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl . '/css/my.css'; ?>"/>
+		<link rel="stylesheet" href="<?php echo Yii::app()->baseUrl . '/css/screen.css'; ?>"/>
+	</head>
+	<body>
+		<h1>Here comes the content:</h1>
+
+		<?php
+
+		echo $content;
+
+		?>
+	</body>
+</html>
+
+
